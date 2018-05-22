9e51b64c33cabb8ef6c24a150d0cbfa1da1933bd
Rohit Menon <s3544222@student.rmit.edu.au>
2018-05-22 16:25:50 +1000

    Jump implemented

--- A3 Game/Assets/playerMovement.cs
+++ A3 Game/Assets/playerMovement.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
-
+    public bool Ground = false;
     public Rigidbody rb;
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    // Use this for initialization
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Ground = true;
 
-        rb.AddForce(1500 * Time.deltaTime, 0, 0);
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+
+        rb.AddForce(180 * Time.deltaTime, 0, 0);
+
+        if(Input.GetKey("space"))
+        {
+            if (Ground)
+            {
+
+                rb.AddForce(000 * Time.deltaTime, 7000 * Time.deltaTime, 0 * Time.deltaTime);
+                Ground = false;
+
+            }
+        }
 	}
 }
